8c3b66d0cbb798ce111000fdbbe9d116e6f18676
asdf <asdf@gh.jkl>
2024-02-07 11:14:26 +0800

    测试自定义规则

--- rewrite/js/7mao.js
+++ rewrite/js/7mao.js
@@ -2,7 +2,21 @@ var url   = $request.url;
 var obj   = JSON.parse($response.body);
 
 if (url.indexOf('/reader-copy-paragraph-all.json') != -1) {
-	let obj = {"data":{"vip_list":["测试2"], "list":["测试2"]}}
-	body = JSON.stringify(obj);
+	obj = {"data":{"vip_list":["测试2"], "list":["测试2"]}}
+} else if(url.indexOf('/api/v1/extra/init') != -1) {
+	if (obj?.data?.reader_floats?.length > 0) {
+		obj.data.reader_floats = [];
+		console.log('设置reader_floats为空');
+	}
+
+	if (obj?.data?.reader_top_banner?.length > 0) {
+		obj.data.reader_top_banner = [];
+		console.log('设置reader_top_banner为空');
+	}
+} else if (url.indexOf('/v1/splash/index') != -1) {
+	if (obj?.init?.is_show_ad) {
+		obj.init.is_show_ad = "0";
+	}
 }
+body = JSON.stringify(obj);
 $done({body});
--- rewrite/js/caiyun.js
+++ rewrite/js/caiyun.js
@@ -21,8 +21,8 @@ var obj   = JSON.parse($response.body);
 let Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNzA1MzMxMTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.h_Cem89QarTXxVX9Z_Wt-Mak6ZHAjAJqgv3hEY6wpps";
 
 if (url.indexOf('/user') != -1 || url.indexOf('/visitors') != -1) {
-		obj.result.token = Token;
-	  body = JSON.stringify(obj);
+	obj.result.token = Token;
+	body = JSON.stringify(obj);
 }
 
 if (url.indexOf('/login_by_code') != -1) {
