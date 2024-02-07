18048bcbac60fd12c4e81afbecc90713a2e3d060
asdf <asdf@gh.jkl>
2024-02-07 16:11:48 +0800

    测试

--- rewrite/js/7mao.js
+++ rewrite/js/7mao.js
@@ -20,6 +20,28 @@ if (url.indexOf("/reader-copy-paragraph-all.json") != -1) {
   if (obj?.init?.is_show_ad) {
     obj.init.is_show_ad = "0";
   }
+
+  if (obj?.init?.voice_free_chapter_count) {
+	obj.init.voice_free_chapter_count = "999";
+  }
+
+  if (obj?.adv?.config) {
+	obj.adv.config.ad_format = "0";
+	obj.adv.config.auto_download_material = "0";
+	obj.adv.config.material_cache_time = 1;
+	obj.adv.config.popuplimit = 0;
+  }
+
+  if (obj?.params) {
+	obj.params.ad_program_switch = 0;
+	obj.params.ad_personal_switch = 0;
+	obj.params.vip_status = 1;
+  }
+  body = JSON.stringify(obj);
+} else if (url.indexOf("/api/v2/init/other-data")) {
+  if (obj?.data?.main_activities) {
+	obj.data.main_activities = null
+  }	
   body = JSON.stringify(obj);
 } else if (url.indexOf("/user/my-center") != -1) {
   body = body
