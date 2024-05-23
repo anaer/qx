de554550ebf9f0bba4cb0cd14d8e3f00ccdd5256
random <a@b.com>
2024-05-23 15:38:11 +0800

    更新pdd规则

--- rewrite/js/pdd.js
+++ rewrite/js/pdd.js
@@ -1,18 +1,12 @@
-let body = $response.body;
-const isResponse = typeof $response !== "undefined";
-
-if(isResponse){
-  let obj = JSON.parse(body);
-  const bottomTabs = [
-    "首页",
-    "分类",
-    "聊天",
-    "个人中心",
-  ];
-  if(obj?.result?.icon_set?.length > 0) {
-	  obj.result.icon_set = [];
-	}
-  if(obj?.result?.buffer_bottom_tabs?.length > 0) {
+var url = $request.url;
+var obj = JSON.parse($response.body);
+if (url.indexOf("/api/alexa/homepage/hub") != -1) {
+  // 首页
+  const bottomTabs = ["首页", "分类", "聊天", "个人中心"];
+  if (obj?.result?.icon_set?.length > 0) {
+    obj.result.icon_set = [];
+  }
+  if (obj?.result?.buffer_bottom_tabs?.length > 0) {
     let newBufferBottomTabs = [];
     for (let tab of obj.result.buffer_bottom_tabs) {
       if (bottomTabs.includes(tab?.title)) {
@@ -21,7 +15,7 @@ if(isResponse){
     }
     obj.result.buffer_bottom_tabs = newBufferBottomTabs;
   }
-  if(obj?.result?.bottom_tabs?.length > 0) {
+  if (obj?.result?.bottom_tabs?.length > 0) {
     let newBottomTabs = [];
     for (let tab of obj.result.bottom_tabs) {
       if (bottomTabs?.includes(tab?.title)) {
@@ -30,8 +24,20 @@ if(isResponse){
     }
     obj.result.bottom_tabs = newBottomTabs;
   }
-  body = JSON.stringify(obj);
-  $done({ body });
-}else{
-  $done();
+
+  // 搜索热词
+  if (obj?.result?.search_bar_hot_query) {
+    obj.result.search_bar_hot_query.hotqs = [];
+    obj.result.search_bar_hot_query.items = [];
+    obj.result.search_bar_hot_query.shade = {};
+  }
+} else if (url.indexOf("/api/caterham/v3/query/new_chat_group") != -1) {
+  // 聊天页
+  if (obj?.data) {
+    obj.data.goods_list = [];
+    obj.data.has_more = false;
+  }
 }
+
+body = JSON.stringify(obj);
+$done({ body });
