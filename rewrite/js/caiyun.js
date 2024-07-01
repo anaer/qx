326dbd3cfc917a1a4647f6aa0bf2ee4401a8c0d3
anaer <4637100+anaer@users.noreply.github.com>
2024-07-01 12:59:27 +0800

    Update caiyun.js

--- rewrite/js/caiyun.js
+++ rewrite/js/caiyun.js
@@ -5,7 +5,7 @@ https://raw.githubusercontent.com/wf021325/qx/master/js/caiyun.js
 */
 var res   = {};
 var url   = $request.url;
-var obj   = JSON.parse($response.body);
+var obj   = JSON.parse(typeof $response != "undefined" && $response.body || null);
 let Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXNzaW9uIjoiNjYyNzQxMzVkYWM3MGMwMDE4YzFlNDBmIiwidXNlcl9pZCI6IjVmNWJmYzU3ZDJjNjg5MDAxNGUyNmJiOCIsInZlcnNpb24iOjIsImV4cCI6MTcyMTYyNDYyOSwidmlwX2V4cGlyZWRfYXQiOjAsImlzcyI6IndlYXRoZXIiLCJpYXQiOjE3MTM4NDg2MjksInN2aXBfZXhwaXJlZF9hdCI6MTg1NjY4NTAzMSwicHJpbWFyeSI6dHJ1ZX0.bBT3vbfATa-LF1G34j4VjPTYtwcKHfG3oHIkFlmg1dY";
 let userId = "5f5bfc57d2c6890014e26bb8";
 
