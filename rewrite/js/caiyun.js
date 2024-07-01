503c66ed9f1474be1b79f46da16b200663fbb610
random <a@b.com>
2024-07-01 13:53:07 +0800

    test

--- rewrite/js/caiyun.js
+++ rewrite/js/caiyun.js
@@ -8,12 +8,13 @@ var obj   = JSON.parse(typeof $response != "undefined" && $response.body || null
 let Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXNzaW9uIjoiNjYyNzQxMzVkYWM3MGMwMDE4YzFlNDBmIiwidXNlcl9pZCI6IjVmNWJmYzU3ZDJjNjg5MDAxNGUyNmJiOCIsInZlcnNpb24iOjIsImV4cCI6MTcyMTYyNDYyOSwidmlwX2V4cGlyZWRfYXQiOjAsImlzcyI6IndlYXRoZXIiLCJpYXQiOjE3MTM4NDg2MjksInN2aXBfZXhwaXJlZF9hdCI6MTg1NjY4NTAzMSwicHJpbWFyeSI6dHJ1ZX0.bBT3vbfATa-LF1G34j4VjPTYtwcKHfG3oHIkFlmg1dY";
 let userId = "5f5bfc57d2c6890014e26bb8";
 
-if (url.indexOf('/user') != -1) {
+if (url.indexOf('/user') != -1 || url.indexOf('/login_by_code') != -1) {
 	obj.result.token = Token;
 	obj.result.is_vip = true;
+	obj.result.vip_expired_at = 3742732800;
 	obj.result.is_xy_vip = true;
-	obj.result.svip_expired_at = 3742732800;
 	obj.result.vip_type = "s";
+	obj.result.svip_expired_at = 3742732800;
 	body = JSON.stringify(obj);
 }
 
@@ -37,10 +38,6 @@ if (url.indexOf('/visitors') != -1) {
 	body = JSON.stringify(obj);
 }
 
-if (url.indexOf('/login_by_code') != -1) {
-	let obj = {"status":"ok","result":{"is_phone_verified":true,"token":Token},"rc":0}
-	body = JSON.stringify(obj);
-}
 
 if(url.includes('v1/activity') != -1) {
 	body = '{"status":"ok","activities":[{"items":[{}]}]}';
