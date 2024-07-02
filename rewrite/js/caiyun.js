bb6f22e0acb0a252d9eb122da12aa9402d11e649
anaer <4637100+anaer@users.noreply.github.com>
2024-07-02 18:20:57 +0800

    更新 caiyun.js

--- rewrite/js/caiyun.js
+++ rewrite/js/caiyun.js
@@ -8,7 +8,6 @@ var obj   = JSON.parse(typeof $response != "undefined" && $response.body || null
 let Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXNzaW9uIjoiNjYyNzQxMzVkYWM3MGMwMDE4YzFlNDBmIiwidXNlcl9pZCI6IjVmNWJmYzU3ZDJjNjg5MDAxNGUyNmJiOCIsInZlcnNpb24iOjIsImV4cCI6MTcyMTYyNDYyOSwidmlwX2V4cGlyZWRfYXQiOjAsImlzcyI6IndlYXRoZXIiLCJpYXQiOjE3MTM4NDg2MjksInN2aXBfZXhwaXJlZF9hdCI6MTg1NjY4NTAzMSwicHJpbWFyeSI6dHJ1ZX0.bBT3vbfATa-LF1G34j4VjPTYtwcKHfG3oHIkFlmg1dY";
 
 if (url.indexOf('/user') != -1 || url.indexOf('/login_by_code') != -1) {
-	obj.result.token = Token;
 	obj.result.is_vip = true;
 	obj.result.vip_expired_at = 3742732800;
 	obj.result.vip_type = "s";
