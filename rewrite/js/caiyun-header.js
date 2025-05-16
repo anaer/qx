fc31a6cae4f2dfb5cf339e5b039a2bf2b352f4c7
random <no@thankyou.com>
2025-05-16 15:36:21 +0800

    调整请求头

--- rewrite/js/caiyun-header.js
+++ rewrite/js/caiyun-header.js
@@ -2,7 +2,8 @@ var res   = {};
 let header = $request.headers;
 
 if (typeof $response === "undefined") {
-  header["device-token"] =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNzA1MzMxMTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.h_Cem89QarTXxVX9Z_Wt-Mak6ZHAjAJqgv3hEY6wpps";
+  const cyTK = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNzA1MzMxMTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.h_Cem89QarTXxVX9Z_Wt-Mak6ZHAjAJqgv3hEY6wpps";
+  header["device-token"] = cyTK;
+  header["Authorization"] = "Bearer " + cyTK;
   $done({ headers: header });
 }
