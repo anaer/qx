476f5aef7a16695e56933b45db44bc19de411a7f
random <r@n.om>
2026-03-17 17:47:23 +0800

    更新规则文件

--- update.bat
+++ update.bat
@@ -4,3 +4,9 @@ d https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Qua
 d https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/Spotify/Spotify.list filter
 d https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/Telegram/Telegram.list filter
 d https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/YouTube/YouTube.list filter
+
+d https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/Twitter/Twitter.list filter
+d https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/Blizzard/Blizzard.list filter
+d https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/Proxy/Proxy.list filter
+
+d https://github.com/blackmatrix7/ios_rule_script/blob/master/rule/QuantumultX/Advertising/Advertising.list filter
